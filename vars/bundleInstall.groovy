a6ed214c3dbd2d2a10f091231023d7cc5c6315e3
Steven Stevanus <sstevanus@kueski.com>
2018-05-24 18:13:30 -0500

    add shell command

--- vars/bundleInstall.groovy
+++ vars/bundleInstall.groovy
@@ -2,7 +2,7 @@ def call() {
   node {
     stage('Bundle Install') {
       echo "Command Executed using the Library"
-      sh '~/.rbenv/shims/bundle install'      
+      ~/.rbenv/shims/bundle install
     }
   }
 }

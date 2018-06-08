a8eff1f9db95ac33df34360ca6d9aa39bb793e7a
Steven Stevanus <sstevanus@kueski.com>
2018-06-08 11:31:53 -0500

    new file

--- vars/getGradleLibraryPath.groovy
+++ vars/getGradleLibraryPath.groovy
@@ -1,4 +1,4 @@
 def call(string) {
 //    string = string.replaceAll('home','var/lib')
-    return string + "@libs/jenkins-shared-library/gradle-scripts"
+    return string + "\@libs/jenkins-shared-library/gradle-scripts"
 }
